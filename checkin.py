bf7128316def34444fdf7aa639ea34649f416c2f
Andrew Cording <drewcording@gmail.com>
2022-03-05 17:59:33 +0000

    First draft of completed checkin

--- checkin.py
+++ checkin.py
@@ -26,26 +26,49 @@ mycursor = mydb.cursor()
 @client.event
 async def on_ready():
     print('checkin started on bot {0.user}'.format(client))
-    
     await client.wait_until_ready()
+    
+    guild = discord.utils.get(client.guilds, name=os.getenv('guild_name'))
+    guest_role = discord.utils.get(guild.roles, name=os.getenv('guest_role'))
+    probation_role = discord.utils.get(guild.roles, name=os.getenv('probation_role'))
+    member_role = discord.utils.get(guild.roles, name=os.getenv('member_role'))
+    mod_role = discord.utils.get(guild.roles, name=os.getenv('mod_role'))
+    announce_channel = discord.utils.get(guild.channels, name=os.getenv('announce_channel'))
+    commands_channel = discord.utils.get(guild.channels, name=os.getenv('commands_channel'))
 
     today = date.today()
     min_probation = today - timedelta(days=int(os.getenv('min_probation')))
     max_probation = today - timedelta(days=int(os.getenv('max_probation')))
 
     mycursor = mydb.cursor()
-    #mycursor.execute(str("SELECT user_id FROM members WHERE join_date='" + str(min_probation) + "'"))
-    mycursor.execute(str("SELECT user_id FROM members WHERE join_date='" + str(today) + "'"))
+    mycursor.execute(str("SELECT user_id FROM members WHERE join_date='" + str(min_probation) + "'"))
+    #mycursor.execute(str("SELECT user_id FROM members WHERE join_date='" + str(today) + "'"))
     min_probation_members = mycursor.fetchall()
     mydb.commit()
 
     mycursor = mydb.cursor()
     mycursor.execute(str("SELECT user_id FROM members WHERE join_date='" + str(max_probation) + "'"))
+    #mycursor.execute(str("SELECT user_id FROM members WHERE join_date='" + str(today) + "'"))
     max_probation_members = mycursor.fetchall()
     mydb.commit()
 
     for member in min_probation_members:
-        print(member)
+        numeric_filter = filter(str.isdigit, str(member))
+        member_numeric = "".join(numeric_filter)
+        await announce_channel.send("<@!" + str(member_numeric) + "> has been on probation for 2 weeks today.")
+
+    for member in max_probation_members:
+        numeric_filter = filter(str.isdigit, str(member))
+        member_numeric = "".join(numeric_filter)
+        await announce_channel.send("<@!" + str(member_numeric) + "> has been on probation for 4 months today and has been demoted to guest.")
+
+        user = discord.utils.get(guild.members, id=int(member_numeric))
+        await user.add_roles(guest_role)
+        await user.remove_roles(probation_role)
+
+        mycursor = mydb.cursor()
+        mycursor.execute("DELETE FROM members WHERE user_id=" + str(member_numeric))
+        mydb.commit()
 
     await client.close()
 
